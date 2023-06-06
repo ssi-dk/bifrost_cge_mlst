9be1f7d17277d0c30e784e91b8d81c5ec3f2aa5a
Karen Loaiza <kloc@ssi.dk>
2023-06-06 17:01:39 +0200

    Replaced dashes _ for dots . in versions for setuptools compatibility and added biopython to install_requires

--- setup.py
+++ setup.py
@@ -2,23 +2,18 @@ from setuptools import setup, find_packages
 
 setup(
     name='bifrost_cge_mlst',
-    version='v2_2_9',
+    version='2.2.9',
+    description='mlst component for Bifrost',
     url='https://github.com/ssi-dk/bifrost_cge_mlst',
-
-    # Author details
     author='Kim Ng',
     author_email='kimn@ssi.dk',
-
-    # Choose your license
     license='MIT',
-
     packages=find_packages(),
-    python_requires='>=3.6',
-
-    package_data={'bifrost_cge_mlst': ['config.yaml', 'pipeline.smk']},
-    include_package_data=True,
-
     install_requires=[
-        'bifrostlib >= 2.1.9'
-    ]
+        'bifrostlib >= 2.1.9',
+        'biopython==1.81'
+    ],
+    python_requires='>=3.6',
+    package_data={'bifrost_cge_mlst': ['config.yaml', 'pipeline.smk']},
+    include_package_data=True
 )
